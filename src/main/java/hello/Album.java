f89c5dc9bcfa6d476ffb4b5a2f070f68a3dc826a
Jakub Wierzchoslawski <jakub.wierzchoslawski@gmail.com>
2017-03-09 18:50:17 +0100

    init

---
+++ src/main/java/hello/Album.java
@@ -0,0 +1,83 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Album properties holder
+ * Core domain object. The one and only!
+ * 
+ * @author Jakub_Wierzchoslawsk
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Album {
+    private String id;
+
+    private String title;
+    private String artist;
+    private String releaseYear;
+    private String genre;
+    private int trackCount;
+    private String albumId;
+    
+    public Album(){}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getTrackCount() {
+		return trackCount;
+	}
+
+	public void setTrackCount(int trackCount) {
+		this.trackCount = trackCount;
+	}
+
+	public String getAlbumId() {
+		return albumId;
+	}
+
+	public void setAlbumId(String albumId) {
+		this.albumId = albumId;
+	}
+    
+    
+    
+}
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,83 @@
+package hello;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 
+ * pcf-demo REST consumer
+ * Many Spring Boot developers always have their main class annotated with
+ * @Configuration, @EnableAutoConfiguration and @ComponentScan. 
+ * Since these annotations are so frequently used together
+ * (especially if you follow the best practices above), 
+ * Spring Boot provides a convenient @SpringBootApplication alternative.
+ * 
+ * for more #goto https://docs.spring.io/spring-boot/docs/current/reference/html/using-boot-using-springbootapplication-annotation.html
+ * 
+ * @author Jakub_Wierzchoslawsk
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		
+		/**
+		 * IMPORTANT NOTICE
+		 * 
+		 * Download cert first and import it to your 
+		 * Java trustcert repository.
+		 * 
+		 * for Java 8 cacerts file can be used and its
+		 * located under %JAVA_HOME%/jre/lib/security/cacerts
+		 * 
+		 * To import downloaded cert to your cert repo use:
+		 * <JAVA_HOME/bin>#keytool.exe -import -file <path_to_cert> -keystore <cacerts_location> -alias "pcf dev certificate"
+		 * 
+		 * Also, when starting PCF DEV use -k flag to import cert to repo
+		 * #cf dev start -k
+		 * 
+		 */
+        System.setProperty("javax.net.ssl.trustStore","c:/Program Files/Java/jdk1.8.0_121/jre/lib/security/cacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+        
+		SpringApplication.run(Application.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			
+			
+			List<Album> albumsResponse =
+			        restTemplate.exchange("http://pcf-demo.local.pcfdev.io/albums",
+			                    HttpMethod.GET, null, new ParameterizedTypeReference<List<Album>>() {
+			            }).getBody();
+			
+			
+			for (Album a: albumsResponse){
+				System.out.println(a.getArtist());
+				log.info(a.getArtist());
+			}
+
+		};
+	}
+}
