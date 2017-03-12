a3e3b7472f36284816f509b4efba0b92f782bf24
jotwu123 <jakub.wierzchoslawski@gmail.com>
2017-03-12 01:03:00 +0100

    eureka + config server

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
